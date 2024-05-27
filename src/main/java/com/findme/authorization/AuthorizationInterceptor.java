135b17f96e8909fec433d5ab96e764f2bea8a828
YodaBikarbona <mihael.peric@hotmail.com>
2024-05-27 19:28:36 +0200

    remove comments.

--- src/main/java/com/findme/authorization/AuthorizationInterceptor.java
+++ src/main/java/com/findme/authorization/AuthorizationInterceptor.java
@@ -24,13 +24,10 @@ public class AuthorizationInterceptor implements HandlerInterceptor {
         if (handler instanceof HandlerMethod handlerMethod) {
             Authorization authorization = handlerMethod.getMethodAnnotation(Authorization.class);
             if (authorization != null) {
-                // Perform token validation and userId extraction logic here
                 String token = request.getHeader("Authorization");
                 if (token == null || token.isEmpty()) {
                     throw new AuthorizationException("Authorization token is missing!");
                 }
-
-                // Your token validation and extraction logic here
                 Claims claims = jwtUtil.getClaimsFromToken(token, Boolean.TRUE);
                 Long userId = Long.parseLong(claims.get("id", String.class));
                 request.setAttribute("userId", userId);
